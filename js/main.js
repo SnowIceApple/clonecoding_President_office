ad7c4fb58712a10ef09edee54f2b5f07234b2b14
SnowIceApple <154928907+SnowIceApple@users.noreply.github.com>
2024-07-18 05:44:57 +0900

    nav 제작중

--- js/main.js
+++ js/main.js
@@ -1,4 +1,15 @@
 $(document).ready(function(){
 
-    
+    $('#nav').on('mouseenter focusin', function(){
+        $(this).addClass('active');
+        $('.megamenu_bg').stop().slideDown(300);
+        $('ul.floor2').stop().slideDown(300);
+    });
+
+    $('#nav').on('mouseleave focusout', function(){
+        $(this).removeClass('active');
+        $('.megamenu_bg').stop().slideUp(300);
+        $('ul.floor2').stop().slideUp(300);
+    });
+
 });
