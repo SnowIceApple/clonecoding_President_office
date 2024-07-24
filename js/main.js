317323e16568a901e04a67a66d1462a28864341b
SnowIceApple <154928907+SnowIceApple@users.noreply.github.com>
2024-07-24 10:23:03 +0900

    240724 오전

--- js/main.js
+++ js/main.js
@@ -1,5 +1,11 @@
 $(document).ready(function(){
 
+    AOS.init({
+      useClassNames: true,
+      initClassName: false,
+      animatedClassName: 'animated',
+    });
+
     $('#nav').on('mouseenter focusin', function(){
         $(this).addClass('active');
         $('.megamenu_bg').stop().slideDown(300);
@@ -65,27 +71,88 @@ $(document).ready(function(){
         duration: 500,
       
         pagination: {
-          el: '.swiper-pagination',
+          el: '.swiper-pagination.pagi_type1',
+          type: 'progressbar',
         },
       
         navigation: {
-          nextEl: '.swiper-button-next',
-          prevEl: '.swiper-button-prev',
+          nextEl: '.sbn1',
+          prevEl: '.sbp1',
         },
       
         scrollbar: {
           el: '.swiper-scrollbar',
         },
 
-        on: {
-            slideChange: function(){
-                var idx = this.realIndex;
-                $('.main_img_slide ul li').eq(idx).addClass('active').siblings().removeClass('active');
-            }
-        }
+        // on: {
+        //     slideChange: function(){
+        //         var idx = this.realIndex;
+        //         $('.main_img_slide ul li').eq(idx).addClass('active').siblings().removeClass('active');
+        //     }
+        // }
       });
 
       swiper1.controller.control = swiper2;
       swiper2.controller.control = swiper1;
 
+      const sections = document.querySelectorAll('section');
+
+      let observer1 = new IntersectionObserver((entries) => {
+        entries.forEach((entry) => {
+          if (entry.isIntersecting) {
+            setTimeout(() => {
+              entry.target.classList.add('view');
+            }, 200);
+          }
+        });
+
+      },
+      );
+
+      sections.forEach((view, index) => {
+          observer1.observe(view);
+      });
+
+      const swiper3 = new Swiper('.pd_slide', {
+        // Optional parameters
+        direction: 'horizontal',
+        loop: false,
+        slidesPerView: 'auto',
+        centeredSlides: true,
+        speed: 600,
+        touchRatio: 0,
+
+        navigation: {
+          nextEl: '.pd_slide_button.next',
+          prevEl: '.pd_slide_button.prev',
+        },
+
+        on: {
+            slideChange: function(){
+                var idx = this.realIndex;
+                $('.pd_top ul li').eq(idx).addClass('active').siblings().removeClass('active');
+                $('.pd_bot > ul > li').eq(idx).addClass('active').siblings().removeClass('active');
+            }
+        }
+
+      });
+
+      $('.pd_slide .swiper-slide').on('click', function(){
+        swiper3.slideTo($(this).index(), 600);
+    });
+
+    const swiper4 = new Swiper('.main_info_slide_cont', {
+      // Optional parameters
+      direction: 'horizontal',
+      loop: true,
+      slidesPerView: 'auto',
+      speed: 300,
+      spaceBetween: 25,
+
+      navigation: {
+        nextEl: '.sbn2',
+        prevEl: '.sbp2',
+      },
+
+    });
 });
